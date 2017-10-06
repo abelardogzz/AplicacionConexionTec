54ffa17c1ef7fabd34747d6031c2be0e9beec643
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-10-06 16:47:32 -0500

    profile.html- Agregado un<p> para poder desplegar informacion ProfileScript- Modificacion de metodos Ajax para mostrar los datos cuando se carga la pagina y al hacer clic en un boton para editar

--- scripts/ProfileScript.js
+++ scripts/ProfileScript.js
@@ -5,44 +5,88 @@ var jsonToSend = {
         };
 
     $.ajax({
-            url:"data/applicationLayer.php",
-            type: "POST", <!--GET|POST|PUT-->
-            data: jsonToSend,
-            dataType: "json",
-            contentType : "application/x-www-form-urlencoded", /** Espesify bc default xml and it reads diferently*/
-            success: function(jsonData){
-                /*On success, it returns an array of objects*/
-                console.log(jsonData);
-                
-                    var newhtml ="";
-                    newhtml += " <b>Name:</b> "+ jsonData.fName + " "+
-                                 jsonData.lName + "</br> "+
-                                " <b>User Name:</b> " + jsonData.username + "</br> "+
-                                " <b>Email:</b> " +jsonData.email + "</br> ";
-                    if(jsonData.gender == "M"){
-                        newhtml += " <b>Gender:</b> Masculine </br>";
-                    }
-                    else{
-                       newhtml += " <b>Gender:</b> Femenine </br>"; 
-                    }
+        url:"data/applicationLayer.php",
+        type: "POST", <!--GET|POST|PUT-->
+        data: jsonToSend,
+        dataType: "json",
+        contentType : "application/x-www-form-urlencoded", /** Espesify bc default xml and it reads diferently*/
+        success: function(jsonData){
+            /*On success, it returns an array of objects*/
+            console.log(jsonData);
+            /*
+                var newhtml ="";
+                newhtml += " <b>Name:</b> "+ jsonData.fName + " "+
+                             jsonData.lName + "</br> "+
+                            " <b>User Name:</b> " + jsonData.username + "</br> "+
+                            " <b>Email:</b> " +jsonData.email + "</br> ";
+                if(jsonData.gender == "M"){
+                    newhtml += " <b>Gender:</b> Masculine </br>";
+                }
+                else{
+                   newhtml += " <b>Gender:</b> Femenine </br>"; 
+                }
 
-                    newhtml+= "<b>Country:</b>" + jsonData.country ;
+                newhtml+= "<b>Country:</b>" + jsonData.country ;
+            */
+                $("#UserProfile").append(newhtml);
+            
 
-                    $("#UserProfile").append(newhtml);
-                
+        },
+        error: function(errMessage){
+            alert("ERROR IN PROFILE");
+                //alert(errorMessage.responseText);
+            alert(errMessage.statusText);
+            console.log(errMessage);
+        }
+    });
 
-            },
-            error: function(errMessage){
-                alert("ERROR IN PROFILE");
-                    //alert(errorMessage.responseText);
-                alert(errMessage.statusText);
-                console.log(errMessage);
-                
-            }
+    $("#EditProfile").on("click",function(){
+
+        var jsonToSend = {
+                "action" : "EDITPROFILE"
+        };
+
+        url:"data/applicationLayer.php",
+        type: "POST", <!--GET|POST|PUT-->
+        data: jsonToSend,
+        dataType: "json",
+        contentType : "application/x-www-form-urlencoded", /** Espesify bc default xml and it reads diferently*/
+        success: function(jsonData){
+            /*On success, it returns an array of objects*/
+            console.log(jsonData);
+            /*
+                var newhtml ="";
+                newhtml += " <b>Name:</b> "+ jsonData.fName + " "+
+                             jsonData.lName + "</br> "+
+                            " <b>User Name:</b> " + jsonData.username + "</br> "+
+                            " <b>Email:</b> " +jsonData.email + "</br> ";
+                if(jsonData.gender == "M"){
+                    newhtml += " <b>Gender:</b> Masculine </br>";
+                }
+                else{
+                   newhtml += " <b>Gender:</b> Femenine </br>"; 
+                }
+
+                newhtml+= "<b>Country:</b>" + jsonData.country ;
+            */
+            
+            //Mensaje de alerta con el resultado
+            //Return para notificar.
+            //window.location.replace("profile.html");
+            
+            
+
+        },
+        error: function(errMessage){
+            alert("ERROR IN PROFILE");
+                //alert(errorMessage.responseText);
+            alert(errMessage.statusText);
+            console.log(errMessage);
+        }
     });
 
 
-
+/*
     $("#HomePage").on("click",function(){
         window.location.replace("home.php");
     });
@@ -50,5 +94,6 @@ var jsonToSend = {
     $("#FriendsPage").on("click",function(){
         window.location.replace("friends.php");
     });
+*/
 
 });
