d3cbdb752bf0be4a57478f92330ccc5659cc5abd
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-10-13 17:00:12 -0500

    Script de BD se modifica adapta para que funcion correctamente

--- data/DBScript.sql
+++ data/DBScript.sql
@@ -17,23 +17,23 @@ CREATE TABLE Proyecto (
 	mDescripcion VARCHAR(70) NOT NULL
 );
 
-CREATE TABLE Orders (
-	orderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	oUserName VARCHAR(15) NOT NULL REFERENCES Users(userName),
-	oTotalPrice INT NOT NULL,
-	oAddress VARCHAR(70) NOT NULL REFERENCES Users(uAddress),
-	oToGo BOOLEAN NOT NULL
-);
-
 CREATE TABLE Comments (
 	ID INT NOT NULL AUTO_INCREMENT,
-	username VARCHAR(50) NOT NULL,
+	IdUsuario INT NOT NULL,
 	comment VARCHAR(140) NOT NULL,
 	fname VARCHAR(30) NOT NULL,
 	lname VARCHAR(30) NOT NULL,
 	email VARCHAR(25) NOT NULL,
 	PRIMARY KEY (ID),
-	FOREIGN Key (username) references Users (userName)
+	FOREIGN Key (IdUsuario) references Users(IdUsuario)
+);
+
+CREATE TABLE UserProyecto(
+	IdUsuario INT NOT NULL,
+	IDproyecto INT NOT NULL,
+	PRIMARY KEY (IdUsuario,IDproyecto),
+	FOREIGN Key (IdUsuario) references Users (IdUsuario),
+	FOREIGN Key (IDproyecto) references Proyecto (mID)
 
 );
 
@@ -43,6 +43,8 @@ VALUES ('1','Luis', 'Flores','Gallardo', 'lucfg', 'password123', 'luis@mail.com'
 INSERT INTO Proyecto(mNombre, mDescripcion)
 VALUES ('Papel Reciclado', 'Se trabajo el desarrollo de un proceso para el reciclaje del papel');
 
-INSERT INTO Comments(username, comment, fname, lname, email)
-VALUES  ('alfredo08', 'I really like this site!', 'Alfredo','Salazar', 'alfredo@mail.com'),
-		('thePam22', 'The design has a greate minimalistic trend.', 'Pamela', 'Rodriguez', 'pamela@mail.com');
+INSERT INTO UserProyecto(IdUsuario,IDproyecto)
+VALUES ('1','1');
+
+INSERT INTO Comments(ID,IdUsuario, comment, fname, lname, email)
+VALUES  ('1','1','alfredo08', 'I really like this site!', 'Alfredo','Salazar', 'alfredo@mail.com')
