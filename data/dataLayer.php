52af3ae2566c6700efc9fe0edf591abc7b9ca080
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-10-23 23:35:02 -0500

    edit_Project - Agrega los metodos, se basa en desplegar el contenido del proyecto que se envia como json al momento de cargar al edicion y se guarda el ID. para despues saber cual proyecyo modificar dataLayer- Recibe los nuevos cambios por POST y los lanza a la BD, DataLayer- recibe los datos, identificando con el project_id hace un chequeo que no haya habido fallas y hace el Update, regresando el resultado aecuado

---
+++ data/dataLayer.php
@@ -0,0 +1,197 @@
+
+
+<?php
+	
+	function connectionToDataBase(){
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		$dbname = "conexionTec2";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		if ($conn->connect_error){
+			return null;
+		}
+		else{
+			return $conn;
+		}
+	}
+
+
+	function attemptProfileService($username){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+			//$userName = $_POST['username'];
+			//$userPassword = $_POST['userPassword'];
+			//PROFILE EXAMPLE 
+			$sql = " SELECT * FROM Users WHERE user_id = '$username' ";
+			$result = $conn->query($sql); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0)//Double check
+			{
+				
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	$response = array('Pnombre' => $row['uPNombre'],
+		    	 					'ApellidoP' => $row['uApellidoP'],
+		    	 					'ApellidoM' => $row['uApellidoM'], 
+		    	 					'email' => $row['uEmail']);   
+			    	//array_push($comments, $response);
+			    	
+
+				}
+				//echo ($response['fName']);
+			    //echo json_encode($response);
+			    $conn-> close();
+			    return array("status" => "SUCCESS","profile" => $response);
+
+			    //echo json_encode($result->fetch_assoc());
+			}
+			else
+			{
+				$conn -> close();
+				return array("status" => "USERNAME NOT FOUND");
+		    	//header('HTTP/1.1 406 User not found'); //Pre-Prepares a json file with mssg
+		        //die("Wrong credentials provided!"); 
+			}
+		}else{
+				$conn -> close();
+				return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+	function attemptEditProfileService($nombre,$appP,$appM,$email){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+
+			$sqlVerifica = "SELECT * FROM Users WHERE uEmail = '$email'";
+			$result = $conn->query($sqlVerifica); 
+
+			//Verifica si no esta registrado ese correo antes
+			if ($result->num_rows > 1)//Double check
+			{
+				$conn -> close();
+				return array("status" => "Ese Correo ya esta registrado!");
+		    	//header('HTTP/1.1 406 User not found'); //Pre-Prepares a json file with mssg
+		        //die("Wrong credentials provided!"); 
+			}
+			else
+			{//Realiza el update de datos
+			//Queda pendiente el campo que hace referencia al registro
+			//Eso lo puedo obtener de las sesiones pero no se cual vamos a usar
+		        $sql = "UPDATE Users SET uPNombre = '$nombre', uApellidoP = '$appP', uApellidoM = '$appM', uEmail = '$email' WHERE uEmail = '$email' ";
+
+		        if (mysqli_query($conn,$sql)){//True si se ejectua correcto
+				    $conn-> close();
+				    $datos = array('Pnombre' => $nombre,
+		    	 					'ApellidoP' => $appP,
+		    	 					'ApellidoM' => $appM, 
+		    	 					'email' => $email);  
+				    return array("status" => "SUCCESS","profile" => $datos);
+				}
+				else{//Error al hacer UPDATE en la BD
+					$conn -> close();
+					return array("status" => "ERROR al Editar");
+				}
+
+			    
+			}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos");
+		}
+	}
+
+	function attemptProjectService($pID){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+			//$userName = $_POST['username'];
+			//$userPassword = $_POST['userPassword'];
+			//PROFILE EXAMPLE 
+			$sql = " SELECT * FROM Projecto WHERE project_id = '$pID' ";
+			$result = $conn->query($sql); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0)//Double check
+			{
+				
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	$response = array('ID' => $row['project_id'],
+		    	 					'Nombre' => $row['pNombre'], 
+		    	 					'Descripcion' => $row['pDescripcion'],
+		    	 					'Area' => $row['pArea']);   
+			    	//array_push($comments, $response);
+			    	
+
+				}
+			    $conn-> close();
+			    return array("status" => "SUCCESS","project" => $response);
+			}
+			else
+			{
+				$conn -> close();
+				return array("status" => "Projecto NOT FOUND");
+			}
+		}else{
+				$conn -> close();
+				return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+	function attemptEditProjectService($pID,$Nombre,$Descripcion,$Area){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+
+			$sqlVerifica = "SELECT * FROM Projecto WHERE project_id = '$pID'";
+			$result = $conn->query($sqlVerifica); 
+
+			//Verifica si no esta registrado ese correo antes
+			if ($result->num_rows <= 0)//Double check
+			{
+				$conn -> close();
+				return array("status" => "El proyecto No esta registrado!");
+		    	//header('HTTP/1.1 406 User not found'); //Pre-Prepares a json file with mssg
+		        //die("Wrong credentials provided!"); 
+			}
+			else
+			{//Realiza el update de datos
+			//Queda pendiente el campo que hace referencia al registro
+			//Eso lo puedo obtener de las sesiones pero no se cual vamos a usar
+		        $sql = "UPDATE Projecto SET pNombre = '$Nombre',pDescripcion = '$Descripcion',pArea = '$Area' WHERE project_id = '$pID' ";
+
+		        if (mysqli_query($conn,$sql)){//True si se ejectua correcto
+				    $conn-> close();
+				    $datos = array('Pombre' => $Nombre,
+		    	 					'email' => $Descripcion,
+		    	 					'Area' => $Area);  
+				    return array("status" => "SUCCESS","projecto" => $datos);
+				}
+				else{//Error al hacer UPDATE en la BD
+					$conn -> close();
+					return array("status" => "ERROR al Editar");
+				}
+
+			    
+			}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos");
+		}
+	}
+
+
+
+?>
---
+++ scripts/edit_Project.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+    //AgregarScripts de sesiones
+    //Carga el perfil para ponerlo en los campos 
+    //Modifica los campos y al darle "save" se actualiza
+
+    var jsonToSend = {
+                "action" : "LOADPROJECT",
+                "projectID" : 1 
+        };
+    $.ajax({
+        url:"../data/applicationLayer.php",
+        type: "POST", <!--GET|POST|PUT-->
+        data: jsonToSend,
+        dataType: "json",
+        contentType : "application/x-www-form-urlencoded", 
+        success: function(jsonData){
+            //On success, it returns an array of objects
+            console.log(jsonData);
+            $("#ieditNombre").val(jsonData.Nombre);
+            $("#ieditDescripcion").val(jsonData.Descripcion);
+            $("#ieditProjectID").val(jsonData.ID); //Este tiene que estar hidden en el HTML
+            $("#ieditArea").val(jsonData.Area); 
+
+                      
+
+        },
+        error: function(errMessage){
+            alert("ERROR al Cargar Proyecto");
+                //alert(errorMessage.responseText);
+            alert(errMessage.responseText);
+            console.log(errMessage);
+        }
+    });
+
+        
+    $("#BtnEdit").on("click",function(){
+        alert("Editando proyecto!");
+        //Datos actializados del usuario para hacer cambios
+        var jsonToSend = {
+                "action" : "EDITPROJECT",
+                "Nombre": $("#ieditNombre").val(),
+                "Descripcion" :  $("#ieditDescripcion").val(),
+                "Area" : $("#ieditArea").val(), 
+                "ID" :  $("#ieditProjectID").val() 
+        };
+        $.ajax({
+            url:"../data/applicationLayer.php",
+            type: "POST", <!--GET|POST|PUT-->
+            data: jsonToSend,
+            dataType: "json",
+            contentType : "application/x-www-form-urlencoded", 
+            success: function(jsonData){
+                //On success, it returns an array of objects
+                console.log(jsonData);
+                if( jsonData.status == "SUCCESS"){
+                    //window.location.replace("profile.html");
+                    alert("Se Edito Projecto con exito!");
+                    console.log(jsonData.projecto);
+                }
+                else{
+                    alert("Error al actualizar los campos Del projecto");
+                    alert(jsonData.status);
+                }
+                
+                //Mensaje de alerta con el resultado
+                //Return para notificar.
+            },
+            error: function(errMessage){
+                alert("ERROR IN Edit Project");
+                    //alert(errorMessage.responseText);
+                alert(errMessage.responseText);
+                console.log(errMessage);
+            }
+        });
+    });
+});
