02f168a0e080e945ddeb2d02d9ec567d8e3dd28f
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-10-23 21:12:20 -0500

    Added Base SQL for testing DB information

---
+++ data/DBScript.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE conexionTec;
+
+CREATE TABLE Users (
+	IdUsuario INT NOT NULL PRIMARY KEY,
+	uPNombre VARCHAR(20) NOT NULL,
+	uApellidoP VARCHAR(20) NOT NULL,
+	uApellidoM VARCHAR(20) NOT NULL,
+	userName VARCHAR(15) NOT NULL ,
+	uPassword VARCHAR(30) NOT NULL,
+	uEmail VARCHAR(25) NOT NULL,
+	TipoDeUsuario VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE Proyecto (
+	mID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	mNombre VARCHAR(70) NOT NULL,
+	mDescripcion VARCHAR(70) NOT NULL
+);
+
+CREATE TABLE Orders (
+	orderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	oUserName VARCHAR(15) NOT NULL REFERENCES Users(userName),
+	oTotalPrice INT NOT NULL,
+	oAddress VARCHAR(70) NOT NULL REFERENCES Users(uAddress),
+	oToGo BOOLEAN NOT NULL
+);
+
+CREATE TABLE Comments (
+	ID INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(50) NOT NULL,
+	comment VARCHAR(140) NOT NULL,
+	fname VARCHAR(30) NOT NULL,
+	lname VARCHAR(30) NOT NULL,
+	email VARCHAR(25) NOT NULL,
+	PRIMARY KEY (ID),
+	FOREIGN Key (username) references Users (userName)
+
+);
+
+INSERT INTO Users(IdUsuario,uPNombre, uApellidoP, uApellidoM, userName, uPassword, uEmail,TipoDeUsuario)
+VALUES ('1','Luis', 'Flores','Gallardo', 'lucfg', 'password123', 'luis@mail.com','AdminProyecto');
+
+INSERT INTO Proyecto(mNombre, mDescripcion)
+VALUES ('Papel Reciclado', 'Se trabajo el desarrollo de un proceso para el reciclaje del papel');
+
+INSERT INTO Comments(username, comment, fname, lname, email)
+VALUES  ('alfredo08', 'I really like this site!', 'Alfredo','Salazar', 'alfredo@mail.com'),
+		('thePam22', 'The design has a greate minimalistic trend.', 'Pamela', 'Rodriguez', 'pamela@mail.com');
