9096c2045d93f1acbe24a80c7b52ca940d874816
Javier Sierra <javier2201@gmail.com>
2017-11-07 16:58:35 -0600

    Modificaciones al script de la BD

--- data/DBScript.sql
+++ data/DBScript.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE conexionTec2;
+CREATE DATABASE conexionTec;
 
 CREATE TABLE Users (
 	user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -24,7 +24,6 @@ CREATE TABLE Projecto (
 	user_id INT NOT NULL,
 	pNombre VARCHAR(70) NOT NULL,
 	pDescripcion VARCHAR(70) NOT NULL,
-	pImage VARCHAR(400) NOT NULL,
 	pArea VARCHAR(50) NOT NULL,
 	Deleted BOOLEAN NOT NULL,
 	pFechaRegistro DATE NOT NULL,
@@ -61,8 +60,8 @@ VALUES 	(1,'Regina','Gallardo','Gallardo','567','reg@mail.com','AdminProyecto',F
 INSERT INTO VirtualSample(virtualSample_id,vsStart_Date,vsEnd_Date,vsCurrent)
 VALUES	(1,'2017-01-17','2017-05-05',FALSE),
 		(2,'2017-08-17','2017-12-05',TRUE);
-
-INSERT INTO Projecto(project_id,virtualSample_id,user_id,pNombre, pDescripcion,pArea,Deleted,pImagen,pImagen2,pVideo)
+        
+INSERT INTO Projecto(project_id,virtualSample_id,user_id,pNombre, pDescripcion,pArea,Deleted,pImagen1,pImagen2,pVideo)
 VALUES 	(1,2,2,'PrepaNet Matematicas','Videojuego para enseñar matematicas',FALSE, 'https://previews.123rf.com/images/fer737ng/fer737ng1004/fer737ng100400018/6929700-Cerca-de-un-antiguo-libro-de-texto-con-las-f-rmulas-de-lgebra-Fondo-de-n-meros-y-letras--Foto-de-archivo.jpg','Tecnologica', NULL, NULL),
 		(2,1,1,'Reciclaje de Papel','Formas practicas de recliclar papel en casa',FALSE,'http://3.bp.blogspot.com/-5bPMewqA-_Q/UjidB_Dcf1I/AAAAAAAAIRo/dFwqI1QrJ7c/s320/papel-reciclado.jpg','Ciencia',NULL, NULL);
 
