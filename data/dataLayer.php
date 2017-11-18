10c0833720ffbf1116d8e92ff0b2a31fd398064a
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-11-18 17:17:57 -0600

    Cambios Datalayer para funciones de admin

--- data/dataLayer.php
+++ data/dataLayer.php
@@ -448,4 +448,166 @@ function attemptInsertComment($user_id, $project_id, $text){
 	$responseStatus = array("status" => "500");
     return array("responseStatus"=>$responseStatus);;
 }
+
+	function attemptCreaVirtualSample($dInicio,$dFin,$Current){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+
+			//$sqlVerifica = "SELECT * FROM virtualsample WHERE vsName = '$Nombre'";
+			//$result = $conn->query($sqlVerifica); 
+
+			//Verifica si no esta registrado ese correo antes
+			//if ($result->num_rows > 0)//Double check
+			//{
+			//		$conn -> close();
+			//		return array("status" => "Ese nombre ya esta registrado!");
+			//	}
+			//	else
+			//{//Realiza el update de datos
+				$sqlUpdate = "UPDATE virtualsample SET vsCurrent = false,Calificacion = false ,Registro = false";
+				//$result = $conn->query($sqlVerifica); 
+		        $sqlInsert = "INSERT INTO VirtualSample(vsStart_Date,vsEnd_Date,vsCurrent,Calificacion,Registro) VALUES ('$dInicio','$dFin',$Current,TRUE,TRUE) ";
+		        //INSERT INTO VirtualSample(vsStart_Date,vsEnd_Date,vsCurrent,Calificacion,Registro) VALUES ('2017-10-8','2017-10-8',True,TRUE,TRUE)
+
+		        if (mysqli_query($conn,$sqlUpdate) && mysqli_query($conn,$sqlInsert)){//True si se ejectua correcto
+				    $conn-> close();
+				    return array("status" => "SUCCESS");
+				}
+				else{//Error al hacer UPDATE en la BD
+					$conn -> close();
+					return array("status" => "ERROR al Crear VS");
+				}
+
+			    
+			//}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos, al crear VS");
+		}
+	}
+
+	function attemptUpdateCalificacion($valor){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+			$sqlUpdate = "UPDATE virtualsample SET Calificacion = $valor WHERE vsCurrent = TRUE ";
+
+	        if (mysqli_query($conn,$sqlUpdate)){//True si se ejectua correcto
+			    $conn-> close();
+			    return array("status" => "SUCCESS");
+			}
+			else{//Error al hacer UPDATE en la BD
+				$conn -> close();
+				return array("status" => "ERROR al Actualizar Calificacion VS");
+			}
+
+			    
+			//}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos, al Actualizar Calificacion VS");
+		}
+	}
+
+	function attemptUpdateRegistro($valor){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+			$sqlUpdate = "UPDATE virtualsample SET Registro = $valor WHERE vsCurrent = TRUE ";
+
+	        if (mysqli_query($conn,$sqlUpdate)){//True si se ejectua correcto
+			    $conn-> close();
+			    return array("status" => "SUCCESS");
+			}
+			else{//Error al hacer UPDATE en la BD
+				$conn -> close();
+				return array("status" => "ERROR al Actualizar Calificacion VS");
+			}
+
+			    
+			//}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos, al Actualizar Calificacion VS");
+		}
+	}
+
+
+	function attemptLoadSamples(){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+			$sql = "SELECT * FROM virtualsample";
+			$result = $conn->query($sql); 
+
+			if ($result->num_rows <= 0){//Double check{
+				$conn -> close();
+				return array("status" => "VS no registrados!");
+		    	//header('HTTP/1.1 406 User not found'); //Pre-Prepares a json file with mssg
+		        //die("Wrong credentials provided!"); 
+			}
+
+	        if ($result->num_rows > 0)//Double check
+			{
+				$vs = array();	
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	$response = array('ID' => $row['virtualSample_id'],
+		    	 					'FechaInicio' => $row['vsStart_Date'], 
+		    	 					'FechaFin' => $row['vsEnd_Date'], 
+		    	 					'Current' => $row['vsCurrent']);   
+			    	array_push($vs, $response);
+			    	
+
+				}
+			    $conn-> close();
+			    return array("status" => "SUCCESS","virtualsample" => $vs);
+			}
+			else
+			{
+				$conn -> close();
+				return array("status" => "VirtualSample NOT FOUND");
+			}
+
+			    
+			//}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos, al Actualizar Calificacion VS");
+		}
+	}
+
+	function attemptUpdateVS($Id){
+			$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+
+				$sqlUpdate = "UPDATE virtualsample SET vsCurrent = false,Calificacion = false ,Registro = false";
+				//$result = $conn->query($sqlVerifica); 
+		        $sqlUpdate2 = "UPDATE virtualsample SET vsCurrent = true WHERE virtualSample_id = '$Id' ";
+		        //INSERT INTO VirtualSample(vsStart_Date,vsEnd_Date,vsCurrent,Calificacion,Registro) VALUES ('2017-10-8','2017-10-8',True,TRUE,TRUE)
+
+		        if (mysqli_query($conn,$sqlUpdate) && mysqli_query($conn,$sqlUpdate2)){//True si se ejectua correcto
+				    $conn-> close();
+				    return array("status" => "SUCCESS");
+				}
+				else{//Error al hacer UPDATE en la BD
+					$conn -> close();
+					return array("status" => "ERROR al cambiar Virtual Sample");
+				}
+
+			    
+			//}
+		}else{//Error de conexion con BD
+				$conn -> close();
+				return array("status" => "Problema de conexion con la Base de datos, al cambiar VS");
+		}
+	}
 ?>
