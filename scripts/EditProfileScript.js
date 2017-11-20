2a3365aef9622f2896661a4676e858e3c3137738
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-11-20 01:12:40 -0600

    Agregacion de Scripts para detalles del perfil

---
+++ scripts/EditProfileScript.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+    //AgregarScripts de sesiones
+    //Carga el perfil para ponerlo en los campos 
+    //Modifica los campos y al darle "save" se actualiza
+
+    var jsonToSend = {
+                "action" : "LOADPROFILE"
+        };
+    $.ajax({
+        url:"../data/applicationLayer.php",
+        type: "POST", <!--GET|POST|PUT-->
+        data: jsonToSend,
+        dataType: "json",
+        contentType : "application/x-www-form-urlencoded", 
+        success: function(jsonData){
+            //On success, it returns an array of objects
+            console.log(jsonData);
+            $("#ieditNombre").val(jsonData.Pnombre);
+            $("#ieditApellidoP").val(jsonData.ApellidoP) ;
+            $("#ieditApellidoM").val(jsonData.ApellidoM );
+            $("#iedituserName").val(jsonData.username );
+            $("#ieditEmail").val(jsonData.email);
+
+                      
+
+        },
+        error: function(errMessage){
+            alert("ERROR IN Load PROFILE");
+                //alert(errorMessage.responseText);
+            alert(errMessage.responseText);
+            console.log(errMessage);
+        }
+    });
+
+        
+    $("#BtnEdit").on("click",function(){
+        alert("Editando proyecto!");
+        //Datos actializados del usuario para hacer cambios
+        var jsonToSend = {
+                "action" : "EDITPROFILE",
+                "Nombre": $("#ieditNombre").val(),
+                "ApellidoP" : $("#ieditApellidoP").val(),
+                "ApellidoM" : $("#ieditApellidoM").val(),
+                "username" :  $("#iedituserName").val(),
+                "email" :  $("#ieditEmail").val()
+        };
+        $.ajax({
+            url:"../data/applicationLayer.php",
+            type: "POST", <!--GET|POST|PUT-->
+            data: jsonToSend,
+            dataType: "json",
+            contentType : "application/x-www-form-urlencoded", 
+            success: function(jsonData){
+                //On success, it returns an array of objects
+                console.log(jsonData);
+                if( jsonData.status == "SUCCESS"){
+                    //window.location.replace("profile.html");
+                    alert("Se Edito con exito!");
+                    console.log(jsonData.profile);
+                }
+                else{
+                    alert("Error al actualizar los campos");
+                    alert(jsonData.status);
+                }
+                
+                //Mensaje de alerta con el resultado
+                //Return para notificar.
+            },
+            error: function(errMessage){
+                alert("ERROR IN Edit");
+                    //alert(errorMessage.responseText);
+                alert(errMessage.responseText);
+                console.log(errMessage);
+            }
+        });
+    });
+});
---
+++ scripts/ProfileScript.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	//Agregar los archivos de Sessiones
+
+
+
+var jsonToSend = {
+                "action" : "LOADPROFILE"
+        };
+
+    $.ajax({
+        url:"data/applicationLayer.php",
+        type: "POST", <!--GET|POST|PUT-->
+        data: jsonToSend,
+        dataType: "json",
+        contentType : "application/x-www-form-urlencoded", /** Espesify bc default xml and it reads diferently*/
+        success: function(jsonData){
+            /*On success, it returns an array of objects*/
+            console.log(jsonData);
+            $('#dshowProfile').append(jsonData.Pnombre);
+            $('#dshowProfile').append(jsonData.ApellidoP);
+            $('#dshowProfile').append(jsonData.ApellidoM);
+            $('#dshowProfile').append(jsonData.username);
+            $('#dshowProfile').append(jsonData.email);
+
+        },
+        error: function(errMessage){
+            alert("ERROR IN Load PROFILE");
+                //alert(errorMessage.responseText);
+            alert(errMessage.statusText);
+            console.log(errMessage);
+        }
+    });
+
+    $("#BtnEdita").on('click',function(){
+        window.location.replace("../user/edit.html");
+    });
+
+});
